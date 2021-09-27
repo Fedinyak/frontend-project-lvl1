20f1a532b26e09d8e145b99c42e9986c24340095
Andrey Fedinyak <fedinyakandrey@gmail.com>
2021-09-27 21:55:51 +0300

    Make brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import getUserAnswerBrainPrime from '../src/getUserAnswerBrainPrime.js';
+
+getUserAnswerBrainPrime('Answer "yes" if given number is prime. Otherwise answer "no".');
