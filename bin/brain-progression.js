4308e94156c1ebd25a827e7d4ba2b9794e861e4c
Andrey Fedinyak <fedinyakandrey@gmail.com>
2021-09-27 17:24:25 +0300

    Make brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import getUserAnswerBrainProgression from '../src/getUserAnswerBrainProgression.js';
+
+getUserAnswerBrainProgression('What number is missing in the progression?');
