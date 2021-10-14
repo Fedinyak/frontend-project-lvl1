2a63bed9eb07d22e82ca9f8bf8c209c3a401b65a
Andrey Fedinyak <fedinyakandrey@gmail.com>
2021-10-14 22:36:36 +0300

    Delete comments

--- src/brainPrimeGame.js
+++ src/brainPrimeGame.js
@@ -41,33 +41,3 @@ const expectedAnswer = generateAnswer(numbers, roundOfGames);
 const brainPrimeGame = () => gameEngine(rules, question, expectedAnswer, roundOfGames);
 
 export default brainPrimeGame;
-
-// import readlineSync from 'readline-sync';
-// import getRandomNumber from './getRandomNumber.js';
-// import getPrimeNumber from './getPrimeNumber.js';
-
-// const getUserAnswerBrainCalc = (rules) => {
-//   console.log('Welcome to the Brain Games!');
-//   const userName = readlineSync.question('May I have your name? ');
-//   console.log(`Hello, ${userName}!`);
-//   console.log(rules);
-
-//   for (let i = 0; i < 3; i += 1) {
-//     const maxRandomNumber = 1000;
-//     const randomNumber = getRandomNumber(maxRandomNumber);
-//     const answer = getPrimeNumber(randomNumber);
-
-//     console.log(`Question: ${randomNumber}`);
-//     const userAnswer = readlineSync.question('Your answer: ');
-
-//     if (answer === userAnswer) {
-//       console.log('Correct!');
-//     } else if (answer !== userAnswer) {
-//       return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${userName}!`);
-//     }
-//   }
-
-//   return console.log(`Congratulations, ${userName}!`);
-// };
-
-// export default getUserAnswerBrainCalc;
