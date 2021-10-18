6bc006495f6480216bbe4ef4619ba30885a1176a
Andrey Fedinyak <fedinyakandrey@gmail.com>
2021-10-18 18:23:56 +0300

    Reduced functions

--- src/games/brainCalcGame.js
+++ src/games/brainCalcGame.js
@@ -1,56 +1,49 @@
-import generateNumbers from '../generateNumbers.js';
+import getRandomNumber from '../getRandomNumber.js';
 import gameEngine from '../index.js';
 
 const rules = 'What is the result of the expression?';
 const maxRandomNumber = 100;
 const maxRandomOperation = 4;
-const roundOfGames = 3;
 
-const firstNumbers = generateNumbers(maxRandomNumber, roundOfGames);
-const secondNumbers = generateNumbers(maxRandomNumber, roundOfGames);
-const operationsNum = generateNumbers(maxRandomOperation, roundOfGames);
-
-const getOperationSymbol = (num, rounds) => {
-  const operation = [];
-  for (let i = 0; i < rounds; i += 1) {
-    if (num[i] === 1) {
-      operation.push('+');
-    } else if (num[i] === 2) {
-      operation.push('-');
-    } else if (num[i] === 3) {
-      operation.push('*');
-    }
-  }
-  return operation;
-};
-
-const operationsSymbol = getOperationSymbol(operationsNum, roundOfGames);
-
-const generateQuestions = (firstNum, secondNum, operation, rounds) => {
-  const questions = [];
-  for (let i = 0; i < rounds; i += 1) {
-    questions.push(`${firstNum[i]} ${operation[i]} ${secondNum[i]}`);
-  }
-  return questions;
-};
-
-const question = generateQuestions(firstNumbers, secondNumbers, operationsSymbol, roundOfGames);
-
-const getCalcNumber = (firstNum, secondNum, operation, rounds) => {
+const generateRandomNumbers = (num, operation) => {
   const result = [];
-  for (let i = 0; i < rounds; i += 1) {
-    if (operation[i] === '+') {
-      result.push(firstNum[i] + secondNum[i]);
-    } else if (operation[i] === '-') {
-      result.push(firstNum[i] - secondNum[i]);
-    } else if (operation[i] === '*') {
-      result.push(firstNum[i] * secondNum[i]);
-    }
+  for (let i = 0; i < 3; i += 1) {
+    const roundNumbers = [];
+    roundNumbers.push(getRandomNumber(num));
+    roundNumbers.push(getRandomNumber(num));
+    roundNumbers.push(getRandomNumber(operation));
+    result.push(roundNumbers);
   }
   return result;
 };
 
-const expectedAnswer = getCalcNumber(firstNumbers, secondNumbers, operationsSymbol, roundOfGames);
-const brainCalcGame = () => gameEngine(rules, question, expectedAnswer, roundOfGames);
+const getQuestionAndCalc = (num) => {
+  const result = [];
+  const operation = num[2];
+  if (operation === 1) {
+    result.push(`${num[0]} + ${num[1]}`);
+    result.push(num[0] + num[1]);
+  } else if (operation === 2) {
+    result.push(`${num[0]} - ${num[1]}`);
+    result.push(num[0] - num[1]);
+  } else if (operation === 3) {
+    result.push(`${num[0]} * ${num[1]}`);
+    result.push(num[0] * num[1]);
+  }
+  return result;
+};
+
+const getQuestionAndCalcArr = (num) => {
+  const result = [];
+  for (let i = 0; i < 3; i += 1) {
+    result.push(getQuestionAndCalc(num[i]));
+  }
+  return result;
+};
+
+const randomNumbers = generateRandomNumbers(maxRandomNumber, maxRandomOperation);
+const question = getQuestionAndCalcArr(randomNumbers);
+
+const brainCalcGame = () => gameEngine(rules, question);
 
 export default brainCalcGame;
