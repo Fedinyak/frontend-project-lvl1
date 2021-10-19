f0e585438ce785a328c0def6525cfce1f53477f3
Andrey Fedinyak <fedinyakandrey@gmail.com>
2021-10-19 12:26:43 +0300

    Reduced functions

--- src/games/brainGcdGame.js
+++ src/games/brainGcdGame.js
@@ -3,50 +3,36 @@ import gameEngine from '../index.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 const maxRandomNumber = 100;
-const roundOfGames = 3;
 
-const generateNumbers = (num, rounds) => {
-  const numbers = [];
-  const arrLength = rounds * 2;
-  for (let i = 0; i < arrLength; i += 1) {
-    numbers.push(getRandomNumber(num));
+const getGreatestCommonDivisor = (num) => {
+  const firstNum = getRandomNumber(num);
+  const secondNum = getRandomNumber(num);
+  const result = [];
+  let divisor = firstNum;
+
+  if (firstNum > secondNum) {
+    divisor = secondNum;
   }
-  return numbers;
-};
 
-const numbersArr = generateNumbers(maxRandomNumber, roundOfGames);
-
-const generateQuestions = (numbers, rounds) => {
-  const questions = [];
-  const arrLength = rounds * 2;
-  for (let i = 0; i < arrLength; i += 2) {
-    questions.push(`${numbers[i]} ${numbers[i + 1]}`);
-  }
-  return questions;
-};
-
-const question = generateQuestions(numbersArr, roundOfGames);
-
-const getGreatestCommonDivisor = (numbers, rounds) => {
-  let divisor;
-  const arrLength = rounds * 2;
-  const GreatestCommonDivisor = [];
-  for (let i = 0; i < arrLength; i += 2) {
-    if (numbers[i] < numbers[i + 1]) {
-      divisor = numbers[i];
-    } else {
-      divisor = numbers[i + 1];
-    }
-    for (let j = divisor; j > 0; j -= 1) {
-      if (numbers[i] % j === 0 && numbers[i + 1] % j === 0) {
-        GreatestCommonDivisor.push(j);
-      }
+  for (let j = divisor; j > 0; j -= 1) {
+    if (firstNum % j === 0 && secondNum % j === 0) {
+      result.push(`${firstNum} ${secondNum}`);
+      result.push(j);
     }
   }
-  return GreatestCommonDivisor;
+
+  return result;
 };
 
-const expectedAnswer = getGreatestCommonDivisor(numbersArr, roundOfGames);
-const brainGcdGame = () => gameEngine(rules, question, expectedAnswer, roundOfGames);
+const getGreatestCommonDivisorArr = (num) => {
+  const result = [];
+  for (let i = 0; i < 3; i += 1) {
+    result.push(getGreatestCommonDivisor(num));
+  }
+  return result;
+};
+
+const question = getGreatestCommonDivisorArr(maxRandomNumber);
+const brainGcdGame = () => gameEngine(rules, question);
 
 export default brainGcdGame;
