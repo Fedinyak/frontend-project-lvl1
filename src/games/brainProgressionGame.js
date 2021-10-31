c0948cd5b44d5a54c8c3d9b4398de19d9f2432b8
Andrey Fedinyak <fedinyakandrey@gmail.com>
2021-10-31 21:02:45 +0300

    Make pure functions

--- src/games/brainProgressionGame.js
+++ src/games/brainProgressionGame.js
@@ -4,42 +4,50 @@ import gameEngine from '../index.js';
 const rules = 'What number is missing in the progression?';
 const maxStartNumber = 10;
 const maxGapProgression = 10;
-const maxLengthProgression = 6;
+const maxLengthProgression = 11;
+const minRandomNumber = 1;
+const minLengthProgression = 5;
 
 const getProgression = (startNum, gap, lengthProg) => {
-  const startNumber = getRandomNumber(startNum);
-  const gapProgression = getRandomNumber(gap);
-  const lengthProgression = getRandomNumber(lengthProg) + 5;
   const result = [];
-  let number = startNumber;
-  for (let j = 0; j < lengthProgression; j += 1) {
+  let number = startNum;
+  for (let j = 0; j < lengthProg; j += 1) {
     result.push(number);
-    number += gapProgression;
+    number += gap;
   }
   return result;
 };
 
-const getQuestion = (startNum, gap, lengthProg) => {
+const getQuestion = (startNum, gap, lengthProg, hiddenNumber) => {
   const progression = getProgression(startNum, gap, lengthProg);
-  const hiddenNumber = getRandomNumber(progression.length);
-  const result = [];
-  const answer = progression[hiddenNumber];
   const question = '..';
   progression[hiddenNumber] = question;
-  result.push(progression.join(' '));
-  result.push(answer);
+  return progression.join(' ');
+};
+
+const getAnswer = (startNum, gap, lengthProg, hiddenNumber) => {
+  const progression = getProgression(startNum, gap, lengthProg);
+  const result = progression[hiddenNumber];
   return result;
 };
 
-const getQuestionArr = (startNum, gap, lengthProg) => {
+const getQuestionAndAnswer = () => {
   const result = [];
   for (let i = 0; i < 3; i += 1) {
-    result.push(getQuestion(startNum, gap, lengthProg));
+    const roundResult = [];
+    const startNumber = getRandomNumber(minRandomNumber, maxStartNumber);
+    const gapProgression = getRandomNumber(minRandomNumber, maxGapProgression);
+    const lengthProgression = getRandomNumber(minLengthProgression, maxLengthProgression);
+    const hiddenNumber = getRandomNumber(minRandomNumber, lengthProgression);
+
+    roundResult.push(getQuestion(startNumber, gapProgression, lengthProgression, hiddenNumber));
+    roundResult.push(getAnswer(startNumber, gapProgression, lengthProgression, hiddenNumber));
+    result.push(roundResult);
   }
   return result;
 };
 
-const question = getQuestionArr(maxStartNumber, maxGapProgression, maxLengthProgression);
+const question = getQuestionAndAnswer();
 const brainProgressionGame = () => gameEngine(rules, question);
 
 export default brainProgressionGame;
