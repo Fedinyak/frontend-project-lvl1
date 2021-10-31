1974a946174172712c4db55e312293e24a71934b
Andrey Fedinyak <fedinyakandrey@gmail.com>
2021-10-31 20:35:38 +0300

    Make pure functions

--- src/games/brainPrimeGame.js
+++ src/games/brainPrimeGame.js
@@ -3,6 +3,7 @@ import gameEngine from '../index.js';
 
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const maxRandomNumber = 1000;
+const minRandomNumber = 1;
 
 const getPrimeNumber = (num) => {
   if (num < 2) {
@@ -17,25 +18,19 @@ const getPrimeNumber = (num) => {
   return 'yes';
 };
 
-const generateAnswer = (num) => {
+const getQuestionAndExpectedAnswer = () => {
   const result = [];
-  const number = getRandomNumber(num);
   for (let i = 0; i < 3; i += 1) {
-    result.push(number);
-    result.push(getPrimeNumber(number));
+    const roundResult = [];
+    const number = getRandomNumber(minRandomNumber, maxRandomNumber);
+    roundResult.push(number);
+    roundResult.push(getPrimeNumber(number));
+    result.push(roundResult);
   }
   return result;
 };
 
-const generateAnswerArr = (num) => {
-  const result = [];
-  for (let i = 0; i < 3; i += 1) {
-    result.push(generateAnswer(num));
-  }
-  return result;
-};
-
-const question = generateAnswerArr(maxRandomNumber);
-const brainPrimeGame = () => gameEngine(rules, question);
+const questionAndExpectedAnswer = getQuestionAndExpectedAnswer();
+const brainPrimeGame = () => gameEngine(rules, questionAndExpectedAnswer);
 
 export default brainPrimeGame;
